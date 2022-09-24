06964b41df37e3e1aab083b6d311e0c7bb2b5da1
Your Name <you@example.com>
2022-09-24 18:33:32 -0500

    Primero

--- 03-JS-III/homework/homework.js
+++ 03-JS-III/homework/homework.js
@@ -8,30 +8,49 @@ function imprimirSumaNumeros() {
     // Imprime la suma de los números del 1 al 10
     // Pista: usa un acumulador
     // Tu código:
-}
+    var suma = 0;
+    for (var i=1; i<11; i++){
+      var suma = suma + i;
+    }
+    return suma;
+};
 
 function encuentraPares(array){
   // Devuelve un arreglo con los pares encontrados
   // en el arreglo de enteros pasado como parámetro
   // Tu código:
-}
+  var numeros = [2,5,6,8,10,3];
+  var numPares = x => x % 2 ===0;
+  var sonPares = numeros.filter(numPares);
+  return sonPares
+};
 
 function elevaAlCuadrado(array){
   // Devuelve un arreglo con cada número del array
   // elevado al cuadrado
   // Tu código:
-}
+  var numeros = [1,4,5,7,6]
+  var alCuad = numeros.map(x => x ** 2)
+  return alCuad
+};
 
 function sumaArray(array){
   // Devuelve el resultado de sumar todos los elementos
   // de un arreglo de enteros dado
   // Tu código:
-}
+  var num = [1,3,5,7,8];
+  var acumulado = 0;
+  num.forEach(x => {
+    acumulado = acumulado + x;
+  });
+  return acumulado;
+};
 
 function numeroDigitos(num){
   // Devuelve el número de dígitos de un número dado
   // Tu código:
-}
+  return num.toString().length
+};
   
   
   // No modificar nada debajo de esta línea
